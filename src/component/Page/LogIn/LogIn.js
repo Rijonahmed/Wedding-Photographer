6e6d7b11aff099b555e6d452c8cb8c3eadf29ddb
Rijonahmed <rijon036@gmail.com>
2022-04-17 12:35:13 +0600

    user creat and email password

--- src/component/Page/LogIn/LogIn.js
+++ src/component/Page/LogIn/LogIn.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import Social from '../Social/Social';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate } from 'react-router-dom';
+import './Login.css'
+
 
 const LogIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+
+  const handleEmailBlur = event => {
+    setEmail(event.target.value)
+  }
+  const handlePasswordBlur = event => {
+    setPassword(event.target.value)
+  }
+  const handleLogIn = event => {
+    event.preventDefault();
+    signInWithEmailAndPassword(email, password)
+  }
+
+  const navigateHandle = event => {
+    navigate('/register');
+  }
+
   return (
     <div>
       <h2 className='text-primary text-center mt-3'>Please Log In</h2>
-      <Form className='w-50 mx-auto mt-3'>
+      <Form onSubmit={handleLogIn} className='w-50 mx-auto mt-3'>
         <Form.Group className="mb-3 " controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
@@ -20,6 +51,8 @@ const LogIn = () => {
           Submit
         </Button>
       </Form>
+
+      <p className='w-50 mx-auto text-center'>New To Best Wedding PhotoGrapher <span className='text-primary cursor-style' onClick={navigateHandle}> New Creat Account</span></p>
 
       <Social></Social>
 
--- src/component/Page/Register/Register.js
+++ src/component/Page/Register/Register.js
@@ -1,27 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import Social from '../Social/Social';
 
 const Register = () => {
-  const navigate = useNavigate
-  const navigateHandleLogin = event => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setpassword] = useState('');
+
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth);
+
+
+  const navigate = useNavigate();
+  const navigateHandleLogin = () => {
     navigate('/login');
 
+  }
+
+  if (user) {
+    navigate('/');
+  }
+  const handleNameBlur = e => {
+    setName(e.target.value);
+  }
+  const handleEmailBlur = e => {
+    setEmail(e.target.value);
+  }
+  const handlePasswordBlur = e => {
+    setpassword(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    createUserWithEmailAndPassword(email, password);
 
   }
+
+
+
+
+
+
   return (
     <div>
       <h2 className='text-primary text-center mt-3'>Please Register Now</h2>
-      <Form className='w-50 mx-auto mt-3'>
-        <Form.Group className="mb-3 " controlId="formBasicEmail">
-          <Form.Control type="text" placeholder="Enter Your Name" />
+      <Form onSubmit={handleSubmit} className='w-50 mx-auto mt-3'>
+        <Form.Group className="mb-3 " controlId="formBasicName">
+          <Form.Control onBlur={handleNameBlur} type="text" placeholder="Enter Your Name" />
         </Form.Group>
         <Form.Group className="mb-3 " controlId="formBasicEmail">
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
@@ -30,7 +68,7 @@ const Register = () => {
           Submit
         </Button>
       </Form>
-      <p className='text-center'>Already creat account purbachal club <span className='text-success ' onClick={navigateHandleLogin}> Log In</span></p>
+      <p className='text-center'>Already Creat account  <span className='text-primary cursor-style' onClick={navigateHandleLogin}> Log In</span></p>
       <Social></Social>
 
     </div>
