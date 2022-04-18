87bd2ea1f4d0f583a91978bb7182a6afc0eefed3
Rijonahmed <rijon036@gmail.com>
2022-04-18 17:17:51 +0600

    last ready to deply commit

--- src/component/Page/NotFound/NotFound.js
+++ src/component/Page/NotFound/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div >
 
-      <img className='w-100' src="https://drudesk.com/sites/default/files/2018-02/404-error-page-not-found.jpg" alt="" />
+      <img className='w-100 vh-100' src="https://drudesk.com/sites/default/files/2018-02/404-error-page-not-found.jpg" alt="" />
 
     </div>
   );
